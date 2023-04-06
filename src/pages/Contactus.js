62fad07d247d239def83ba5e6e019f274767b481
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-04-07 00:08:18 +0530

    don some changes

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -22,8 +22,11 @@ function Contactus() {
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                         </svg>
                                         <div class="ml-4 text-md tracking-wide font-semibold w-40">
-                                            Acme Inc, Street, State,
-                                            Postal Code
+                                            Sector-27, Mihan Rehabilitation Colony
+
+                                            Khapri, Nagpur - 441108
+
+
                                         </div>
                                     </div>
 
@@ -32,7 +35,7 @@ function Contactus() {
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                         </svg>
                                         <div class="ml-4 text-md tracking-wide font-semibold w-40">
-                                            +44 1234567890
+                                            8459795840
                                         </div>
                                     </div>
 
@@ -41,7 +44,7 @@ function Contactus() {
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                         </svg>
                                         <div class="ml-4 text-md tracking-wide font-semibold w-40">
-                                            info@acme.org
+                                            gcoenmesa@gmail.com
                                         </div>
                                     </div>
                                 </div>
