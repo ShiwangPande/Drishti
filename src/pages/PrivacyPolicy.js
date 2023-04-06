40eb7655d3fefbe9b035dd58f6f3e156f353b49e
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-04-06 23:05:13 +0530

    don some changes

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -2,39 +2,40 @@ import React from 'react'
 
 function PrivacyPolicy() {
   return (
-
-    <div className='container max-w-screen-md m-auto bg-blue-950 text-white'>
-      <h1>Privacy Policy</h1>
-      <p>We take your privacy seriously. To better protect your privacy we provide this privacy policy notice explaining the way your personal information is collected and used.</p>
-      <h1>Collection of Routine Information</h1>
-      <p>This website tracks basic information about their users. This information includes, but is not limited to, IP addresses, browser details, timestamps and referring pages. None of this information can personally identify specific users to this website. The information is tracked for routine administration and maintenance purposes.
-      </p>
-
-
-      <h1>Collection of Personal Information</h1>
-      <p>This website tracks basic information about their users. This information includes, but is not limited to, IP addresses, browser details, timestamps and referring pages. None of this information can personally identify specific users to this website. The information is tracked for routine administration and maintenance purposes.</p>
-      <h1>Collection of Personal Information</h1>
-      <p>We collect personal information from you when you register with us, subscribe to our newsletter, respond to a survey or fill out a form. When registering with us, you provide us with your name, email address, password, and a phone number. You may, however, choose to provide us with other information including address, billing information, or other data that maybe linked directly or indirectly to your account. </p>
+    <div className='max-w-screen-md m-auto  bg-blue-950 text-white'>
+      <div className='container max-w-screen-md m-auto'>
+        <h1>Privacy Policy</h1>
+        <p>We take your privacy seriously. To better protect your privacy we provide this privacy policy notice explaining the way your personal information is collected and used.</p>
+        <h1>Collection of Routine Information</h1>
+        <p>This website tracks basic information about their users. This information includes, but is not limited to, IP addresses, browser details, timestamps and referring pages. None of this information can personally identify specific users to this website. The information is tracked for routine administration and maintenance purposes.
+        </p>
 
 
-      <h1>Cookies
-      </h1>
-      <p>Where necessary, this website uses cookies to store information about a visitor's preferences and history in order to better serve the user and/or present the user with customized content.</p>
-      <h1>Advertisement and Other Third Parties</h1>
-      <p>Advertising partners and other third parties may use cookies, scripts and/or web beacons to track user activities on this website in order to display advertisements and other useful information. Such tracking is done directly by the third parties through their own servers and is subject to their own privacy policies. This website has no access or control over these cookies, scripts and/or web beacons that may be used by third parties.</p>
-      <h1>Links to Third Party Websites</h1>
-      <p>
-        We have included links on this website for your use and reference. We are not responsible for the privacy policies on these websites. You should be aware that the privacy policies of these websites may differ from our own.   </p>
-      <h1>Security</h1>
-      <p>The security of your personal information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security. </p>
+        <h1>Collection of Personal Information</h1>
+        <p>This website tracks basic information about their users. This information includes, but is not limited to, IP addresses, browser details, timestamps and referring pages. None of this information can personally identify specific users to this website. The information is tracked for routine administration and maintenance purposes.</p>
+        <h1>Collection of Personal Information</h1>
+        <p>We collect personal information from you when you register with us, subscribe to our newsletter, respond to a survey or fill out a form. When registering with us, you provide us with your name, email address, password, and a phone number. You may, however, choose to provide us with other information including address, billing information, or other data that maybe linked directly or indirectly to your account. </p>
 
-      <h1>Changes To This Privacy Policy</h1>
-      <p>This Privacy Policy is effective as of 6 April 2023 and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page. We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.
-      </p>
 
-      <h1>Contact Information</h1>
-      For any questions or concerns regarding the privacy policy, please send us an email to gcoenmesa@gmail.com.
-      Drishti 2023 | MESA | GCOEN
+        <h1>Cookies
+        </h1>
+        <p>Where necessary, this website uses cookies to store information about a visitor's preferences and history in order to better serve the user and/or present the user with customized content.</p>
+        <h1>Advertisement and Other Third Parties</h1>
+        <p>Advertising partners and other third parties may use cookies, scripts and/or web beacons to track user activities on this website in order to display advertisements and other useful information. Such tracking is done directly by the third parties through their own servers and is subject to their own privacy policies. This website has no access or control over these cookies, scripts and/or web beacons that may be used by third parties.</p>
+        <h1>Links to Third Party Websites</h1>
+        <p>
+          We have included links on this website for your use and reference. We are not responsible for the privacy policies on these websites. You should be aware that the privacy policies of these websites may differ from our own.   </p>
+        <h1>Security</h1>
+        <p>The security of your personal information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security. </p>
+
+        <h1>Changes To This Privacy Policy</h1>
+        <p>This Privacy Policy is effective as of 6 April 2023 and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page. We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.
+        </p>
+
+        <h1>Contact Information</h1>
+        For any questions or concerns regarding the privacy policy, please send us an email to gcoenmesa@gmail.com.
+        Drishti 2023 | MESA | GCOEN
+      </div>
       <style jsx>{`
         h1 {
           font-size: 1.5rem;
