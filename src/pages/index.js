abfff5a90f6247e3b608932c8d26da2d0f9aa872
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-04-07 17:28:06 +0530

    don some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,7 +100,7 @@ export default function Home() {
       <div className='body1'>
         <Head>
           <title>Drishti 2k23</title>
-          <link rel="icon" href="/logo-color.svg" />
+          <link rel="icon" href="/logo-no-background.svg" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
