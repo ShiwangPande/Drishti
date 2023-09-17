1fd177862bfb96c330e8da54e7bc8c3c82b43001
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-09-17 19:14:11 +0530

    first commit

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -27,6 +27,26 @@ function PrivacyPolicy() {
           We have included links on this website for your use and reference. We are not responsible for the privacy policies on these websites. You should be aware that the privacy policies of these websites may differ from our own.   </p>
         <h1>Security</h1>
         <p>The security of your personal information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security. </p>
+        <h1>Event Registration</h1>
+
+        <p>Once you have registered for an event, you cannot cancel your registration.
+          Drishti does not provide refunds or accept any cancellation requests for event registrations.
+          In the event that you are unable to participate in a registered event, your registration fee will not be refunded.
+          Exceptions</p>
+        <p> In exceptional circumstances where an event is canceled by Drishti due to unforeseen circumstances, a full refund of the registration fee will be provided to registered participants.
+          If there are changes to the schedule or format of an event due to unavoidable reasons, registered participants will be informed, and alternative arrangements will be made.
+        </p>
+        <h1>Contact Us</h1>
+        <p> If you have any questions or concerns regarding this policy or your registration, please feel free to contact our team at [Contact Email Address] or [Contact Phone Number].
+        </p>
+
+        <h1> Acceptance of Policy</h1>
+        <p> By registering for an event on the Drishti website, you acknowledge that you have read, understood, and accepted our no-refund and no-cancellation policy.
+        </p>
+
+        <p> We appreciate your understanding and commitment to making Drishti a successful and memorable experience for all participants.
+        </p>
+        <p>  Thank you for your support!</p>
 
         <h1>Changes To This Privacy Policy</h1>
         <p>This Privacy Policy is effective as of 6 April 2023 and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page. We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.
