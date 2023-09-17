38bbb6a0bcc239ad6b8371c9cd7a2ba178e57643
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-09-17 19:16:23 +0530

    first commit

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -27,6 +27,9 @@ function PrivacyPolicy() {
           We have included links on this website for your use and reference. We are not responsible for the privacy policies on these websites. You should be aware that the privacy policies of these websites may differ from our own.   </p>
         <h1>Security</h1>
         <p>The security of your personal information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security. </p>
+        <h1> Registration Fees</h1>
+        <p>Registration fees for Drishti events are non-refundable.</p>
+
         <h1>Event Registration</h1>
 
         <p>Once you have registered for an event, you cannot cancel your registration.
