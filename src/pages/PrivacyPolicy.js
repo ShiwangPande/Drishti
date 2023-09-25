05500d1eb00c9635f48b38a27769706b617f874c
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-09-25 18:23:49 +0530

    first commit

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -31,15 +31,23 @@ function PrivacyPolicy() {
         <p>Registration fees for Drishti events are non-refundable.</p>
         <h1>Payment Currency</h1>
         <p> We accept payment for registration fees exclusively in Indian Rupees (INR). Please ensure that all payments are made in INR to complete the registration process. We do not accept any other currency for registration fees. If you have any questions or concerns regarding payment methods, please contact our customer support team for assistance.</p>
+        <h1> Pricing Policy:</h1>
 
-        <h1>Event Registration</h1>
+        <p> At Drishti Mechanical Tech Fest, we strive to offer fair and competitive pricing for our events. The registration fees for each of our eight events may vary and will be clearly outlined on our website and registration platform. We are committed to providing a valuable experience for all participants.
+        </p>
+        <h1> Refund Policy:</h1>
 
-        <p>Once you have registered for an event, you cannot cancel your registration.
-          Drishti does not provide refunds or accept any cancellation requests for event registrations.
-          In the event that you are unable to participate in a registered event, your registration fee will not be refunded.
-          Exceptions</p>
-        <p> In exceptional circumstances where an event is canceled by Drishti due to unforeseen circumstances, a full refund of the registration fee will be provided to registered participants.
-          If there are changes to the schedule or format of an event due to unavoidable reasons, registered participants will be informed, and alternative arrangements will be made.
+        <p>  Refunds for Event Cancellation: In the event that Drishti Mechanical Tech Fest organizers cancel any of the scheduled events, participants who have registered for the canceled event(s) will receive a full refund of their registration fees.
+
+          Refunds for Non-Attendance: Please note that we do not provide refunds if a registered participant fails to attend any of the events they have registered for. We allocate resources and make arrangements based on the number of registered participants, and thus, the registration fee is non-refundable for no-shows.
+        </p>
+        <h1> Cancellation Policy:</h1>
+        <p>
+          Once a participant has registered for any event at the Drishti Mechanical Tech Fest, they are not allowed to unregister or cancel their registration. We maintain a strict no-cancellation policy to ensure effective event planning and execution.
+
+          We understand that unforeseen circumstances can arise, but we encourage participants to carefully consider their availability and commitment before registering for any of our events. By registering, participants acknowledge and agree to abide by this no-cancellation policy.
+
+          If you have any questions or need further clarification regarding our pricing, refund, or cancellation policies, please feel free to contact our event organizers. We look forward to providing an exciting and enriching experience at the Drishti Mechanical Tech Fest, and we appreciate your understanding of our policies to help us deliver a successful event.
         </p>
         <h1>Contact Us</h1>
         <p> If you have any questions or concerns regarding this policy or your registration, please feel free to contact our team at [gcoenmesa@gmail.com] or [+91 8459795840].
@@ -54,7 +62,7 @@ function PrivacyPolicy() {
         <p>  Thank you for your support!</p>
 
         <h1>Changes To This Privacy Policy</h1>
-        <p>This Privacy Policy is effective as of 6 April 2023 and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page. We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.
+        <p>This Privacy Policy is effective and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page. We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.
         </p>
 
         <h1>Contact Information</h1>
